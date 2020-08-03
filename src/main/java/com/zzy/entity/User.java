09378411dc2b28b58f57ccd225c22fb99d08dcec
zhangzhonghua <583705252@163.com>
2020-08-03 10:39:30 +0800

    v1.2

--- src/main/java/com/zzy/entity/User.java
+++ src/main/java/com/zzy/entity/User.java
@@ -1,6 +1,26 @@
 package com.zzy.entity;
 
+import java.util.Date;
+
 public class User {
     private String name;
     private  Integer age;
+    private Date birday;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birday=" + birday +
+                '}';
+    }
 }
